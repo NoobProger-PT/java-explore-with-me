261ac3a6f6e9e8b620b5e997560a1f283df0cc35
PATAPOLIS <dovgynjenya@yandex.ru>
2023-01-06 00:27:49 +0300

    5.2

--- stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
+++ stats-server/src/main/java/ru/practicum/repository/StatsRepository.java
@@ -1,8 +1,6 @@
 package ru.practicum.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import ru.practicum.dto.ViewStatsDto;
 import ru.practicum.model.EndPointHit;
 
 import java.time.LocalDateTime;
@@ -10,18 +8,5 @@ import java.util.List;
 
 public interface StatsRepository extends JpaRepository<EndPointHit, Long> {
 
-    List<EndPointHit> findAllByUriContainingIgnoreCaseAndTimeStampAfterAndTimeStampBefore(String uris,
-                                                                                          LocalDateTime time1,
-                                                                                          LocalDateTime time2);
-
     List<EndPointHit> findAllByTimeStampBetween(LocalDateTime start, LocalDateTime end);
-
-    Integer countByUri(String name);
-
-    @Query(value = "SELECT s.app, s.uri, COUNT(DISTINCT s.ip) FROM stats AS s WHERE s.timestamp BETWEEN ?1 and ?2",
-            nativeQuery = true)
-    List<EndPointHit> findByUniqIp1(LocalDateTime start, LocalDateTime end);
-
-    @Query(value = "SELECT * FROM stats AS s WHERE s.timestamp BETWEEN ?2 and ?3, ", nativeQuery = true)
-    List<EndPointHit> findByUrisAndUniqIp(List<String> uris, LocalDateTime start, LocalDateTime end);
 }
